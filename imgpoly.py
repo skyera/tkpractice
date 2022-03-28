d003579e536865519fb8677505dd6f55e649bc04
Zhigang <zgliu2@gmail.com>
2022-03-27 19:00:02 -0700

    add panedwindow

--- imgpoly.py
+++ imgpoly.py
@@ -5,8 +5,8 @@ import tkinter.ttk as ttk
 
 
 class ImageGalleryFrame(tk.Frame):
-    def __init__(self, parent):
-        super().__init__()
+    def __init__(self, parent, **options):
+        super().__init__(**options)
         self.make_widgets()
     
     def make_widgets(self):
@@ -22,23 +22,54 @@ class ImageGalleryFrame(tk.Frame):
         self.stext.pack(fill=tk.BOTH, expand=True)
 
 
-class MainFrame(tk.Frame):
+class ImagePolyFrame(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.make_widgets()
 
     def make_widgets(self):
-        self.pw = ttk.PanedWindow(orient=tk.HORIZONTAL)
-        self.pw.pack(fill=tk.BOTH, expand=True)
+        self.make_info_label()
+        self.make_canvas()
+    
+    def make_info_label(self):
+        self.info_label = tk.Label(self, text='Canvas playground', bg='gray90', bd=2)
+        self.info_label.pack(fill=tk.X, padx=5, pady=5)
+    
+    def make_canvas(self):
+        self.canvas = tk.Canvas(self, bg='white')
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+
+
+class MainFrame(tk.Frame):
+    def __init__(self, parent, **options):
+        super().__init__(parent, **options)
+        self.make_widgets()
+    
+    def make_panedwindow(self):
+        self.pw = tk.PanedWindow(self, orient=tk.HORIZONTAL)
+        self.pw.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    def make_widgets(self):
+        self.make_info_panel()
+        self.make_panedwindow()
         self.make_left_pane()
         self.make_right_pane()
+
+    def make_info_panel(self):
+        group = tk.LabelFrame(self, padx=5, pady=5, text='Info')
+        group.pack(side=tk.TOP, padx=10, pady=10, fill=tk.X)
+        tk.Label(group, text='Path').grid(row=0, column=0)
+
     
     def make_left_pane(self):
-        self.left_pane = ImageGalleryFrame(self.pw)
+        self.left_pane = ImageGalleryFrame(self.pw, width=100)
+        self.left_pane.pack(fill=tk.BOTH, expand=True)
         self.pw.add(self.left_pane)
 
     def make_right_pane(self):
-        pass
+        self.right_pane = ImagePolyFrame(self.pw)
+        self.right_pane.pack(fill=tk.BOTH, expand=True)
+        self.pw.add(self.right_pane)
 
 
 class View(tk.Tk):
